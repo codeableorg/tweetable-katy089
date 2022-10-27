5c4d6e198621a013a25be8d35223802da7e52b1b
Camilo-J <josecamilo1902@outlook.com>
2022-10-26 20:57:18 -0500

    [WIP] Add controller user and its views

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,7 +6,7 @@ class Tweet < ApplicationRecord
 
   # Associations
   belongs_to :user
-  belongs_to :replied_to, class_name: "Tweet", optional: true, counter_cache: true
+  belongs_to :replied_to, class_name: "Tweet", optional: true, counter_cache: :replies_count
   has_many :replies, class_name: "Tweet",
                      foreign_key: "replied_to_id",
                      dependent: :destroy,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "tweets#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tweets
 end
