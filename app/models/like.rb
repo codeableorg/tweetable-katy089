a408bbc6f35bb5e7a8cd4716ea75df837f2a943e
Camilo-J <josecamilo1902@outlook.com>
2022-10-26 19:19:29 -0500

    Add validation for table like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
+
+  # Validations
+  validates :user, uniqueness: { scope: :tweet, message: "and tweet combination already taken"}
+  # Associations
   belongs_to :user
   belongs_to :tweet, counter_cache: true
 end
