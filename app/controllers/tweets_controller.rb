5c1ab21678435ecff72ceb24662bae802394db2d
Camilo-J <josecamilo1902@outlook.com>
2022-10-27 18:12:10 -0500

    Add methods edit, create and update for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,24 +17,29 @@ class TweetsController < ApplicationController
     @tweet_got = Tweet.find(params[:id])
   end
 
-  def new
-  end
-
   def create
     @tweet_new = Tweet.new(tweet_params)
-    @tweet_new.user_id = 1
+    @tweet_new.user_id = current_user.id
     @tweet_new.save
 
     redirect_to root_path
   end
 
   def edit
+    @tweet_got = Tweet.find(params[:id])
+    @tweet_parent = @tweet_got.replied_to
   end
 
   def update
+    @tweet_new = Tweet.find(params[:id])
+
+    @tweet_new.update(tweet_params)
+
+    redirect_to root_path
   end
 
   def destroy
+
   end
 
   private
