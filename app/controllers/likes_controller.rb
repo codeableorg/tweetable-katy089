75db759898b9aefc9feefeb7c52d2ddf1b8559c7
Camilo-J <josecamilo1902@outlook.com>
2022-10-28 18:16:24 -0500

    Add method to delete and give a like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,4 +6,12 @@ class LikesController < ApplicationController
 
     redirect_to root_path
   end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @like_got = Like.find_by(user: current_user, tweet: @tweet)
+    @like_got.destroy
+
+    redirect_to root_path
+  end
 end
