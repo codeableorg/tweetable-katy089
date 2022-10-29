8be4c168a91fd33606ecb044aae0de522dae0df4
Camilo-J <josecamilo1902@outlook.com>
2022-10-29 14:24:03 -0500

    Delete  comments for tweet controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,12 +5,6 @@ class TweetsController < ApplicationController
     @tweet_new = Tweet.new
     @like_new = Like.new
 
-    # Select those tweets belonging to current_user (only if it's logged in). Used to filter those who can be edited or deleted. 
-    # Select all for admins 
-    # @user_tweets = current_user ? current_user.tweets : [] 
-    # @user_tweets = current_user ? policy_scope( Tweet ) : []
-    # @user_tweets = policy_scope( Tweet )
-    # @user_likes = current_user ? current_user.liked_tweets : []
   end
 
   def show
